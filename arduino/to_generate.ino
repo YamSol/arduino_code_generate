9617d8a65215d006e1084249cea1c11a03095428
Yam Sol Britto Bertamini <16283636659@tco.lan>
2022-05-02 18:09:02 -0300

    update .gitignore and to_generate

--- arduino/to_generate.ino
+++ arduino/to_generate.ino
@@ -1,29 +1,58 @@
 #define led 12
-#define 
+
+//times to
+#define HYPHEN_COUNT 3
+#define DOT_COUNT 1
+#define SPACE_COUNT 3
+#define SLASHE_COUNT 5
+
+#define DELAY_BASE 400     //ms
+#define DELAY_INTERVAL 200 //ms
 
 void setup()
 {
   pinMode(12, OUTPUT);
-
 }
+
+boolean primeira;
+String strn = "&morse&";
 
 void loop()
 {
+  if (primeira)
+  {
 
-  boolean primeira;
-  String strn = "&morse&";
+    for (int i = 0; i <= strn.length(); i++)
+    {
 
-  for(int i=0; i<=strn.length(); i++){
-    
-    if(strn[i] == '-'){
-      digitalWrite(led, HIGH);
-      delay(1000);  
-    }
+      if (strn[i] == '-')
+      {
+        digitalWrite(led, HIGH);
+        delay(DELAY_BASE * HYPHEN_COUNT);
+      }
 
-    if(strn[i] == '.'){
+      else if (strn[i] == '.')
+      {
+        digitalWrite(led, HIGH);
+        delay(DELAY_BASE * DOT_COUNT);
+      }
+
+      else if (strn[i] == ' ')
+      {
+        digitalWrite(led, LOW);
+        delay(DELAY_BASE * SPACE_COUNT);
+      }
+
+      else if (strn[i] == '/')
+      {
+        digitalWrite(led, LOW);
+        delay(DELAY_BASE * SLASHE_COUNT);
+      }
+
       digitalWrite(led, LOW);
-      delay(1000);
+      delay(DELAY_INTERVAL);
     }
+    primeira = false;
   }
   delay(5000);
 }
